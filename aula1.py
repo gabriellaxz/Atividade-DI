1e1169ca16fa5e7c8140a4d66337603005e14a20
mabriella <79538335+gabriellaxz@users.noreply.github.com>
2024-04-10 10:25:33 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,13 @@
+idade1 = 20
+idade2= 35
+somaIdades = idade1 + idade2
+
+'''Operações'''
+
+subtracao = idade1 - idade2
+soma = idade1 + idade2
+divisao = idade1 / idade2
+
+'''Operações multiplas'''
+
+2 * (idade1 + idade2)
